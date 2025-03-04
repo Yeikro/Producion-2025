995c70f0bd3502ffda2a1ade5aa04b19e53e3a95
Daniela <daniela.mb0709@gmail.com>
2025-03-04 11:53:33 -0500

    Movimiento con contoles

--- Assets/Scripts/ControlDePersonaje.cs
+++ Assets/Scripts/ControlDePersonaje.cs
@@ -1,27 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ControlDePersonaje : MonoBehaviour
 {
     public Animator animaciones;
+    public InputActionProperty controlMover;
+    public InputActionProperty controlSalto;
+    public InputActionProperty controlAtaque;
+    public InputActionProperty controlDefender;
+    public InputActionProperty controlAgacharse;
+    Vector2 movimiento;
+    public float velSuavisada;
+    private void Start()
+    {
+        controlMover.action.Enable();
+        controlSalto.action.Enable();
+        controlAtaque.action.Enable();
+        controlDefender.action.Enable();
+        controlAgacharse.action.Enable();
+        controlSalto.action.performed += _ => Saltar();
+        controlAtaque.action.performed += _ => Ataque();
+        
+
+    }
+    public void Saltar()
+    {
+        animaciones.SetTrigger("Jump");
+    }
+    public void Ataque()
+    {
+        animaciones.SetTrigger("Atack");
+    }
+    
+
     void Update()
     {
-        animaciones.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
-        animaciones.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        movimiento = Vector2.Lerp(movimiento, controlMover.action.ReadValue<Vector2>(), velSuavisada * Time.deltaTime);
 
-        if(Input.GetKey(KeyCode.Space))
-        {
-            animaciones.SetTrigger("Jump");
-        }
-        if (Input.GetMouseButtonDown(0))
-        {
-            animaciones.SetTrigger("Atack");
-        }
-        if (Input.GetMouseButtonDown(1))
-        {
-            animaciones.SetTrigger("Defens");
-        }
+        animaciones.SetFloat("Horizontal", movimiento.x);
+        animaciones.SetFloat("Vertical", movimiento.y);
+
+        animaciones.SetBool("Defens", controlDefender.action.ReadValue<float>()>0.5f);
+        animaciones.SetBool("Down" , controlAgacharse.action.ReadValue<float>()>0.5f);
+
+
     }
 
 }
