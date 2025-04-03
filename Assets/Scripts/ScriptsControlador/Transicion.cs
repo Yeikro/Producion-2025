e35ccfef7294f17cf558b4ffc0de5b03bf2e173c
Daniela <daniela.mb0709@gmail.com>
2025-04-03 11:55:24 -0500

    Organizacion enemigp

--- Assets/Scripts/ScriptsControlador/Transicion.cs
+++ Assets/Scripts/ScriptsControlador/Transicion.cs
@@ -1,18 +1,29 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Transicion : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Image imagenObjetivo;  // Imagen cuyo fillAmount es el objetivo
+    public Image imagenDelay;     // Imagen cuyo fillAmount se actualizará suavemente
+    public float velocidad;       // Velocidad de la transición
+
+    private void Start()
     {
-        
+        // Inicia la corutina que actualiza constantemente el fillAmount
+        StartCoroutine(ActualizarFillAmountSuavizado());
     }
 
-    // Update is called once per frame
-    void Update()
+    // Esta corutina actualizará el fillAmount de imagenDelay constantemente
+    private IEnumerator ActualizarFillAmountSuavizado()
     {
-        
+        while (true) // Se ejecutará constantemente
+        {
+            // Interpolamos suavemente entre el fillAmount actual de imagenDelay y el de imagenObjetivo
+            imagenDelay.fillAmount = Mathf.Lerp(imagenDelay.fillAmount, imagenObjetivo.fillAmount, Time.deltaTime * velocidad);
+
+            // Esperamos hasta el siguiente frame
+            yield return null;
+        }
     }
 }
