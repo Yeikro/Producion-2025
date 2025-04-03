e64a93293922fd78137a4e8ba009ed8f058efb62
santi <santiagoaguirreb6@gmail.com>
2025-04-03 10:47:14 -0500

    Personalizar nombre y vida ui

--- Assets/Scripts/ScriptsControlador/Vida.cs
+++ Assets/Scripts/ScriptsControlador/Vida.cs
@@ -3,23 +3,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class Vida : MonoBehaviour
 {
+    [SerializeField] Image healthbarImage;
+    [SerializeField] GameObject ui;
+    PhotonView PV;
+
     public float vidaInicial;
     public float vidaActual;
     public UnityEvent eventoMorir;
     public bool estaMuerto = false;
 
+    void Awake()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+
     void Start()
     {
         Reiniciar();
+
+        if (!PV.IsMine)
+            Destroy(ui);
     }
 
 
     public void CausasDaño(float cuanto)
     {
+        if (!PV.IsMine)
+            return;
+
         vidaActual -= cuanto;
+
+        healthbarImage.fillAmount = vidaActual / vidaInicial;
+
         if (vidaActual < 0)
         {
             print("Muerto!! ->" + gameObject.name);
