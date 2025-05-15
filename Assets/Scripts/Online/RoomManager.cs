eb1c7388c98227b26b151ebed0edd54ef38da316
santi <santiagoaguirreb6@gmail.com>
2025-05-15 10:36:45 -0500

    Ajuste UI

--- Assets/Scripts/Online/RoomManager.cs
+++ Assets/Scripts/Online/RoomManager.cs
@@ -34,7 +34,7 @@ public class RoomManager : MonoBehaviourPunCallbacks
 
     void OnSceneLoad(Scene scene, LoadSceneMode loadSceneMode)
     {
-        if(scene.buildIndex == 1)
+        if(scene.buildIndex == 2)
         {
             PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerManager"), Vector3.zero, Quaternion.identity);
         }
